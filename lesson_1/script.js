5e6bf498423921a0b447851b4c7250bdd8caca12
Alex Leontiev <nailbiter@dtws-work.in>
2020-04-28 19:27:14 +0900

    commit

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -9,12 +9,14 @@ ctx.closePath();
 
 ctx.beginPath();
 ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
+ctx.fillStyle = "rgba(255,0,255,0.1)";
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
 ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.strokeStyle = "rgba(1.0, 1.0, 1.0, 0.5)";
 ctx.stroke();
 ctx.closePath();
+
+// test test
