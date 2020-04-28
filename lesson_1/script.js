6105fb591d64e08340629f8d4dcb740760e408fa
mikhaylo leontiev <mike@gmail.com>
2020-04-28 14:04:14 +0300

    пкуууууууууууууууууу

--- lesson_1/script.js
+++ lesson_1/script.js
@@ -2,14 +2,14 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
+ctx.rect(20, 20 50, 50);
+ctx.fillStyle = "rgba(255, 0, 255, 1)";
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
 ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
+ctx.fillStyle = "rgba(255, 255, 0, 1)";
 ctx.fill();
 ctx.closePath();
 
